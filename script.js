1e5d0cd55e02373a3c396e639f56ae76d3f4dd82
Charles Effiong <charleseffiong28@gmail.com>
2025-02-08 17:49:35 +0100

    to-do-list

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const addButton = document.getElementById('add-task-btn');
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
+
+    function addTask(){
+        const taskInput = document.getElementById('task-input');
+        const taskText = taskInput.Value.trim()
+    }
+
+})
