5ed2e1cd2a144448d5dd4b51746317556c1f2a7a
Charles Effiong <charleseffiong28@gmail.com>
2025-02-09 12:26:16 +0100

    to-do-list

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
-document.addEventListener("DOMContentLoaded", function(){
-    addTask();
-})
-document.addEventListener('DOMContentLoaded', function() {
-    const addButton = document.getElementById('add-task-btn');
-        addButton.addEventListener('clicked', addTask);
+document.addEventListener("DOMContentLoaded", function () {
+  addTask();
+});
+document.addEventListener("DOMContentLoaded", function () {
+    function loadTasks (){
+        const storedTasks = JSON.parse(localStorage.getItem('task') || '[]')
+    }
+  const addButton = document.getElementById("add-task-btn");
+  addButton.addEventListener("clicked", addTask);
+  const taskInput = document.getElementById("task-input");
+  taskInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  });
+  const taskList = document.getElementById("task-list");
+
+  function addTask(taskText, save = true) {
+    if (save) {
+        const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+        storedTasks.push(taskText);
+        localStorage.setItem('tasks', JSON.stringify(storedTasks));
+    }
     const taskInput = document.getElementById("task-input");
-        taskInput.addEventListener('keypress', function (event) {
-        if (event.key ==="Enter"){
-            addTask();
-        };
-    });
-    const taskList = document.getElementById("task-list");
+    const taskText = taskInput.value.trim();
+    if (taskText === "") {
+      alert("Please enter a task.");
+    } else {
+      const li = document.createElement("li");
+      li.textContent = taskText;
+      const removeButton = document.createElement("button");
+      removeButton.textContent = "Remove";
+      removeButton.classList.add = "remove-btn";
+      removeButton.onclick = function () {
+        li.remove();
+      };
 
-    function addTask(){
-        const taskInput = document.getElementById('task-input')
-        const taskText = taskInput.value.trim()
-        if  (taskText === ""){
-            alert('Please enter a task.')
-        } else {
-            const li = document.createElement('li')
-            li.textContent = taskText;
-            const removeButton = document.createElement('button');
-            removeButton.textContent = 'Remove';
-            removeButton.classList.add = 'remove-btn';
-            removeButton.onclick = function () {
-                li.remove(); 
-            };
-   
-            li.appendChild(removeButton);
-            taskList.appendChild(li);
-            taskInput.value = '';
-
+      li.appendChild(removeButton);
+      taskList.appendChild(li);
+      taskInput.value = "";
     }
-        
-
-    
-
-    }
-        
-      
-})
+  }
+});
